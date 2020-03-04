c88dc9f19ba57a2591f6905711d3ff6ca1a49e44
Scott Mosher <scott@scottmosherphotography.com>
2020-03-03 21:40:36 -0700

    Started on "Adding a Search"

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -20,9 +20,20 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string searchString)
         {
-            return View(await _context.Movie.ToListAsync());
+            // Create the linq query
+            var movies = from m in _context.Movie
+                         select m;
+
+            // If searchString isn't empty or whitespace, run the query
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+
+            // Now return the results to the view
+            return View(await movies.ToListAsync());
         }
 
         // GET: Movies/Details/5
