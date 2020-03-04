56f2644aed8142286d26d922d82bda0f7dd0be6e
Scott Mosher <scott@scottmosherphotography.com>
2020-03-03 21:34:30 -0700

    Finished Controller Actions

--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -2,10 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+using MvcMovie;
+using MvcMovie.Data;
 
 namespace MvcMovie
 {
@@ -13,7 +19,26 @@ namespace MvcMovie
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<MvcMovieContext>();
+                    context.Database.Migrate();
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while trying to seed the DB");
+                }
+            }
+
+                host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
